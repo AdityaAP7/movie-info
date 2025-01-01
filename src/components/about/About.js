6ac536009653cf89e06035ed2740126594b1e3d1
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-01 23:12:31 +0530

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeaderC from "./../header/HeaderC";
 import img from "./about2.jpg";
 import { Col, Container, Row } from "react-bootstrap";
-
+import { Col, Container, Row } from "react-bootstrap";
 const About = () => {
   return (
     <div className="about">
